5c09e22deab825995fd9855641c2ea0738b2e101
himanshukumar003 <147289870+0himanshu3@users.noreply.github.com>
2024-10-11 01:05:40 +0530

    Updated knuth-morris-pratt.js in accordance with the es6 .
    Just changed the code in es6 format keeping the logic same.

--- src/searching/knuth-morris-pratt.js
+++ src/searching/knuth-morris-pratt.js
@@ -1,18 +1,18 @@
 (function (exports) {
   'use strict';
 
-  var kmp = (function () {
-    function builtKMPTable(str) {
-      var res = [];
-      var len;
-      var front;
-      var end;
-      var found;
-      for (var i = 1; i <= str.length; i += 1) {
+  const kmp = (() => {
+
+    const builtKMPTable = (str) => {
+      const res = [];
+      let len, front, end, found;
+
+      for (let i = 1; i <= str.length; i += 1) {
         front = Math.max(1, i - ((res[i - 2] || 0) + 1));
         end = Math.min(i - 1, (res[i - 2] || 0) + 1);
         found = false;
         len = 0;
+
         while (end >= 1 && front <= i && !found) {
           if (str.substring(0, end) === str.substring(front, i)) {
             found = true;
@@ -22,10 +22,12 @@
             front += 1;
           }
         }
+
         res[i - 1] = len;
       }
+
       return res;
-    }
+    };
 
     /**
      * Knuth–Morris–Pratt algorithm. Searches for the position of
@@ -33,9 +35,8 @@
      *
      * @example
      *
-     * var indexOf = require('path-to-algorithm/src/searching/'+
-     * 'knuth-morris-pratt').kmp;
-     * console.log(indexOf('hello', 'll')); // 2
+     * import { kmp } from './path-to-algorithm/src/searching/knuth-morris-pratt';
+     * console.log(kmp('hello', 'll')); // 2
      *
      * @public
      * @module searching/knuth-morris-pratt
@@ -45,21 +46,25 @@
      * where the specified substring occurs for the first
      * time, or -1 if it never occurs.
      */
-    function indexOf(str, substr) {
+    const indexOf = (str, substr) => {
       if (str === substr) {
         return 0;
       }
-      var table = builtKMPTable(substr);
-      var i = 0;
-      var j = 0;
+
+      const table = builtKMPTable(substr);
+      let i = 0;
+      let j = 0;
+
       while (i < str.length) {
         if (str[i] === substr[j]) {
           i += 1;
           j += 1;
         }
+
         if (j === substr.length) {
           return i - j;
         }
+
         if (i < str.length && str[i] !== substr[j]) {
           if (j > 0 && table[j - 1] !== 0) {
             j = table[j - 1];
@@ -69,10 +74,12 @@
           }
         }
       }
+
       return -1;
-    }
+    };
+
     return indexOf;
-  }());
+  })();
 
   exports.kmp = kmp;
 
